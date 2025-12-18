807bdb0e368fe8683931533a72a28b9c6aa4090a
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-12-19 02:39:05 +0300

    Add routes for the new pages

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import Product from "../pages/Product";
+import Pricing from "../pages/Pricing";
+import PageNotFound from "../pages/PageNotFound";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/Product" element={<Product />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
