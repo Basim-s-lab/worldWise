3a587814fd54073e514ec36d25270edcad0680af
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-12-21 04:46:17 +0300

    Add nested routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,11 @@ export default function App() {
         <Route path="/Product" element={<Product />}></Route>
         <Route path="/pricing" element={<Pricing />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/app" element={<AppLayoute />}></Route>
+        <Route path="/app" element={<AppLayoute />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="/*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
