ffc80f2b66fae1955eb54f7cbd0d22925faf424b
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-12-25 06:00:00 +0300

    Add useSearchParams hook to read query strings

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,18 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>
+        Position:{lat},{lng}
+      </p>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 45 })}>
+        Change position
+      </button>
+    </div>
+  );
 }
