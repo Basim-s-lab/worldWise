0052617f7069fad3eb11ba8fb0b4d1089704a431
ShiroYaksh90 <aljazaeribasim@gmail.com>
2026-01-05 23:56:55 +0300

    Add routes for citites & cities.id

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import cities from "./data/cities.json" with { type: 'json'}
 
 const app = express();
 
 const port = 5000;
+
+app.get("/api/cities", (req, res) => {
+    console.log(cities.cities)
+    res.json(cities)
+})
+
+app.get("/api/cities/:id", (req, res) => {
+    let id = Number(req.params.id)
+    const city = cities.cities.find((c) => c.id === id)
+    res.json(city)
+})
 
 app.listen(port, () => {
     console.log(`Server is runing on port: ${port}`)
