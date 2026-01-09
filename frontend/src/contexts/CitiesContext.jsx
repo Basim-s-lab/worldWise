0018307d2d1426eb9c776e5f43e3e720485124c0
ShiroYaksh90 <aljazaeribasim@gmail.com>
2026-01-09 18:44:03 +0300

    Add CORS mode and credentials to fetch requests in CitiesContext

--- frontend/src/contexts/CitiesContext.jsx
+++ frontend/src/contexts/CitiesContext.jsx
@@ -13,7 +13,10 @@ function CitiesProvider({ children }) {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
+        const res = await fetch(`${BASE_URL}/cities`, {
+          mode: "cors",
+          credentials: "include",
+        });
         const data = await res.json();
         setCities(data);
       } catch {
@@ -28,7 +31,10 @@ function CitiesProvider({ children }) {
   async function getCity(id) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        mode: "cors",
+        credentials: "include",
+      });
       const data = await res.json();
       SetCurrentCity(data);
     } catch {
