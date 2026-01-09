d3c04516cd6d4d82eeff37a841908e5ed891a70b
ShiroYaksh90 <aljazaeribasim@gmail.com>
2026-01-09 18:33:44 +0300

    Refactor CORS middleware to allow multiple origins for API requests

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ connectionDb();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: "https://world-wise-4jrx.vercel.app/" }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://world-wise-4jrx.vercel.app", "https://worldwise-90.netlify.app");
+    next();
+})
 
 app.use("/api/cities", citiesRoutes)
 
